70f792c3dae530d86996c6582297eb6f489243ff
Viraj Shah <vjshah3@illinois.edu>
2022-08-05 17:07:58 -0700

    added_wt_in_path

--- multistyle_utils.py
+++ multistyle_utils.py
@@ -63,7 +63,7 @@ def get_savepath(config):
     for name in config['names']:
         dirname = dirname + f'_{ref_code[strip_path_extension(name)]}'
 
-    dirname = dirname + f'_ctx_wt_{config["ctx_loss_w"]}_n_iter_{config["num_iter"]}'
+    dirname = dirname + f'_ctx_wt_{config["ctx_loss_w"]}_n_iter_{config["num_iter"]}_type_{config["weight_type"]}'
     dirpath = os.path.join(config['savepath'], dirname)
 
     if not os.path.exists(dirpath):
@@ -81,7 +81,7 @@ def save_single_image(mode,i,j, img, config, use_wandb):
     for name in config['names']:
         dirname = dirname + f'_{ref_code[strip_path_extension(name)]}'
 
-    dirname = dirname + f'_ctx_wt_{config["ctx_loss_w"]}_n_iter_{config["num_iter"]}'
+    dirname = dirname + f'_ctx_wt_{config["ctx_loss_w"]}_n_iter_{config["num_iter"]}_type_{config["weight_type"]}'
 
     dirpath = os.path.join(config['savepath'], dirname, mode)
 
@@ -124,7 +124,7 @@ def save_batch_images(batch_num, images, config, use_wandb):
     for name in config['names']:
         dirname = dirname + f'_{ref_code[strip_path_extension(name)]}'
 
-    dirname = dirname + f'_ctx_wt_{config["ctx_loss_w"]}_n_iter_{config["num_iter"]}'
+    dirname = dirname + f'_ctx_wt_{config["ctx_loss_w"]}_n_iter_{config["num_iter"]}_type_{config["weight_type"]}'
     dirpath = os.path.join(config['savepath'], dirname, 'full_random')
 
     if not os.path.exists(dirpath):
